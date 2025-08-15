2d78f54cbed9b14ca36cfe0d23bf741d9544182b
Thamizhselvan05 <gthamizhselvang@gmail.com>
2025-08-15 11:42:29 +0530

    Commit

---
+++ C2TC_CoreJava_Thamizh/src/Day2/Executor.java
@@ -0,0 +1,18 @@
+package Day2;
+
+public class Executor {
+	
+	public static void main(String[] args)  {
+		
+		Person p = new Person();
+		p.setId(10);
+		p.setName("thamizh");
+		p.setAge(20);
+		p.setCity("puducherry");
+		
+		
+		System.out.println("Id: " 
+		);
+	}
+	
+}
