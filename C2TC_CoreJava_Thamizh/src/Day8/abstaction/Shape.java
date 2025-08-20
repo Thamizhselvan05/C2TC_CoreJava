06ed6162ca87258696145d4d40877cefbce03f64
Thamizhselvan05 <gthamizhselvang@gmail.com>
2025-08-20 21:01:56 +0530

    Commit

---
+++ C2TC_CoreJava_Thamizh/src/Day8/abstaction/Shape.java
@@ -0,0 +1,16 @@
+package Day8.abstaction;
+
+public abstract class Shape {
+	
+	protected float area;
+    
+   
+	 abstract void calArea();
+	 
+	 
+	 void show()
+	 {
+		 System.out.println("Area of shape is "+area);
+	 }
+
+}
