e269cc4c54d7865b4821661e1b32d0df1689f2bf
Thamizhselvan05 <gthamizhselvang@gmail.com>
2025-08-25 14:44:31 +0530

    Commit

---
+++ C2TC_CoreJava_Thamizh/src/Day12/UncheckedException.java
@@ -0,0 +1,13 @@
+package Day12;
+
+public class UncheckedException {
+	
+	public static void main(String[] args) {
+		int intArray[];
+		
+			intArray = new int[] { 1, 2, 3, 4 };
+			System.out.println(intArray[6]);
+			
+	}
+
+}
---
+++ C2TC_CoreJava_Thamizh/src/Day19/IStatement.java
@@ -0,0 +1,7 @@
+package Day19;
+
+@FunctionalInterface 
+interface IStatement {
+	public String show();
+	
+}
