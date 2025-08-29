c2f0e62f16071145bd24973bcfcb4e7fc9a157a4
Thamizhselvan05 <gthamizhselvang@gmail.com>
2025-08-29 14:37:53 +0530

    Commit

---
+++ C2TC_CoreJava_Thamizh/src/Day14/synchronization/Bank.java
@@ -0,0 +1,9 @@
+package Day14.synchronization;
+
+public interface Bank {
+	int MINBAL=5000;
+	static final int DAILY_LIMIT=25000;
+	public abstract void deposit(int amt) throws DepositLimitExceedsException;
+	void withdraw(int amt)throws InsufficientBalanceException; 
+
+}
